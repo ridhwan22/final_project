48b9d631ac1253db317059bdfced133b0afa7f0c
ridhwan22 <mrnasir2@illinois.edu>
2019-04-20 21:01:02 -0500

    run 1

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,46 +1,94 @@
 package com.example.final_project;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public final Button[] buttons = {
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+    };
+
+    private final String[][] answers = {{"A"}, {"B"}, {"C"}, {"D"}, {"A"}};
+    private final String[][] choices = {
+            {"A", "B", "C", "D"},
+            {"A", "B", "C", "D"},
+            {"A", "B", "C", "D"},
+            {"A", "B", "C", "D"},
+            {"A", "B", "C", "D"}
+    };
+    private static final int[] imageIDs = {
+            R.drawable.a,
+            R.drawable.abc,
+            R.drawable.pic1,
+            R.drawable.abcd,
+            R.drawable.cs125
+    };
+    boolean hasStarted = false;
+    boolean hasEnded = false;
+    private static int score = 0;
+    private static int questionNo = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Creating a dropdown list that matches the id **/
-        Spinner days = (Spinner) findViewById(R.id.weekDays);
-        //Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> dropDownAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.weekDays));
-        //Making dropDownAdapter as a drop down list**/
-        dropDownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Set the spinner days to the adaptor dropDownAdaptor**/
-        days.setAdapter(dropDownAdapter);
-
-        // Making a responsible drop down when each item is selected
-        days.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            /**.
-             * responsible drop down list
-             * @param parent
-             * @param view
-             * @param position position of each drop down list item
-             * @param id
-             */
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
+
+    //when we move to the next question, showPicture is called again to show another new picture
+    public void showPicture(int questionNo) {
+        ImageView toShow = findViewById(R.id.imageView);
+        toShow.setImageResource(imageIDs[questionNo]);
+    }
+
+    public void showChoices(int questionNo) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setText(choices[questionNo][i]);
+        }
+    }
+    //when button is clicked, call checkAnswer
+    public void buttonOnClick(View v) {
+        Button button = (Button) v;
+        int num = 0;
+        for (int i = 0; i < buttons.length; i++) {
+            if (button.equals(buttons[i])) {
+                num = i;
+            }
+        }
+        if (choices[questionNo][num].equals(answers[questionNo])) {
+            buttons[num].setBackgroundColor(Color.GREEN);
+            score++;
+            questionNo++;
+            Toast.makeText(this,"RIGHT!",Toast.LENGTH_SHORT);
+            nextQuestion(questionNo);
+        } else {
+            buttons[num].setBackgroundColor(Color.RED);
+            score--;
+            questionNo++;
+            Toast.makeText(this,"WRONG!",Toast.LENGTH_SHORT);
+            nextQuestion(questionNo);
+        }
+    }
+
+    public void nextQuestion(int questionNo) {
+        if (questionNo >= imageIDs.length) {
+            endGame();
+        }
+        showPicture(questionNo);
+        showChoices(questionNo);
+    }
+
+    public void endGame() {
+        String finalScore = Integer.toString(getScore());
+        Toast.makeText(this,"CONGRATS! You Got " + finalScore,Toast.LENGTH_LONG);
+    }
+    public int getScore() { return score; }
 }
---
+++ app/src/main/java/com/example/final_project/callMeMaybe.java
@@ -0,0 +1,10 @@
+package com.example.final_project;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.Button;
+
+
+public class callMeMaybe extends MainActivity{
+}
